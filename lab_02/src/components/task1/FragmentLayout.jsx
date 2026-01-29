8d3962b4859e2022840f29c9816ef61140627630
Zangar-Rakhmatulla <rakhmatulla.zangar@mail.ru>
2026-01-30 04:21:10 +0500

    Initial commit

---
+++ lab_02/src/components/task1/FragmentLayout.jsx
@@ -0,0 +1,16 @@
+// src/components/task1/FragmentLayout.jsx
+export default function FragmentLayout() {
+  return (
+    <>
+      <header>
+        <h1>Fragment Layout</h1>
+      </header>
+      <main>
+        <p>Бұл тек фрагменттерді қолдану.</p>
+      </main>
+      <footer>
+        <small>© 2026</small>
+      </footer>
+    </>
+  );
+}
